227bcc552f1d09efe9e0821a40c8dff7d3c1d45a
borhanGit <mdborhan413@gmail.com>
2023-07-12 17:55:41 +0600

    brands

--- resources/views/frontend/include/banner.blade.php
+++ resources/views/frontend/include/banner.blade.php
@@ -4,25 +4,34 @@
             <div class="swiper-container qrt-brands-slider" style="overflow: visible">
               <div class="swiper-wrapper">
                 <div class="swiper-slide">
-                  <img src="img/brands/b1.svg" alt="brand">
+                  <img src="{{ asset('img/brands/BMS.png') }}" alt="brand">
                 </div>
                 <div class="swiper-slide">
-                  <img src="img/brands/b2.svg" alt="brand">
+                  <img src="{{ asset('img/brands/Dinner In the Sky.png') }}" alt="brand">
                 </div>
                 <div class="swiper-slide">
-                  <img src="img/brands/b3.svg" alt="brand">
+                  <img src="{{ asset('img/brands/Dry Express.png') }}" alt="brand">
                 </div>
                 <div class="swiper-slide">
-                  <img src="img/brands/b4.svg" alt="brand">
+                  <img src="{{ asset('img/brands/Golden Poly.png') }}" alt="brand">
                 </div>
                 <div class="swiper-slide">
-                  <img src="img/brands/b5.svg" alt="brand">
+                  <img src="{{ asset('img/brands/Handiz.png') }}" alt="brand">
                 </div>
                 <div class="swiper-slide">
-                  <img src="img/brands/b6.svg" alt="brand">
+                  <img src="{{ asset('img/brands/Next IT.png') }}" alt="brand">
                 </div>
                 <div class="swiper-slide">
-                  <img src="img/brands/b7.svg" alt="brand">
+                  <img src="{{ asset('img/brands/One Touch.png') }}" alt="brand">
+                </div>
+                <div class="swiper-slide">
+                  <img src="{{ asset('img/brands/OVIA.png') }}" alt="brand">
+                </div>
+                <div class="swiper-slide">
+                  <img src="{{ asset('img/brands/Polok Group.png') }}" alt="brand">
+                </div>
+                <div class="swiper-slide">
+                  <img src="{{ asset('img/brands/UAE.png') }}" alt="brand">
                 </div>
               </div>
             </div>
